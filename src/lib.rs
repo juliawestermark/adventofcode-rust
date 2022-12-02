6f0fcbc7567e4b893d46c464b77e1cd3210eda74
Julia Westermark <julia.westermark@cubist.eu>
2022-12-02 23:38:13 +0100

    First commit. Day 1 and 2

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::{
+    env,
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+    process,
+};
+
+
+pub fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+pub fn parse_args() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("not enough arguments");
+    }
+    
+    let filename: String = match args[1].parse() {
+        Ok(filename) => filename,
+        Err(_) => {
+            eprintln!("Input file not specified`");
+            process::exit(1);
+        }
+    };
+    filename
+}
---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::env;
+use std::process::Command;
+
+pub const ANSI_ITALIC: &str = "\x1b[3m";
+pub const ANSI_BOLD: &str = "\x1b[1m";
+pub const ANSI_RESET: &str = "\x1b[0m";
+
+fn startup(day: i32) {
+    println!();
+    println!("{}--- 🎄 Day {:02} 🎄 ---{}", ANSI_BOLD, day, ANSI_RESET);
+    println!();
+}
+
+fn ending() {
+    println!();
+    println!("{}--- 🎄🎄🎄🎄🎄🎄 ---{}", ANSI_BOLD, ANSI_RESET);
+}
+
+struct Config {
+    day: i32,
+    input_filename: String,
+}
+
+// fn help() {
+//     println!("usage:
+// match_args <string>
+//     Check whether given string is the answer.
+// match_args {{increase|decrease}} <integer>
+//     Increase or decrease given integer by one.");
+// }
+
+fn parse_config(args: &[String]) -> Config {
+
+    let input_filename;
+
+    if args.len() < 2 {
+        panic!("not enough arguments");
+    }
+
+    let day: i32 = match args[1].parse() {
+        Ok(day) => {
+            if day < 1 || day > 25 {
+                panic!("argument day is not in interval");
+            }
+            day
+        },
+        Err(_)=> panic!("argument day is invalid"),
+    };
+
+    if args.len() < 3 {
+        input_filename = format!("src/inputs/input{:02}.txt", day).to_string();
+    }
+    else {
+        match args[2].as_str() {
+            "result" => {
+                input_filename = format!("src/inputs/input{:02}.txt", day).to_string();
+            },
+            "test" => {
+                input_filename = format!("src/examples/testinput{:02}.txt", day).to_string();
+            },
+            _ => {
+                input_filename = format!("src/inputs/input{:02}.txt", day).to_string();
+            },
+        }
+    }
+
+    Config {day, input_filename}
+
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+    let input_filename = config.input_filename;
+    let day = config.day;
+    let dday = format!("{:02}", day);
+
+    let cmd = Command::new("cargo")
+        .args(["run", "--bin", &format!("day{}", dday), &input_filename])
+        .output()
+        .unwrap();
+    
+    let output = String::from_utf8(cmd.stdout).unwrap();
+    let vec: Vec<&str> = output.trim().split("\n").collect();
+    
+    // PRINTS
+    startup(day);
+    for part in vec {
+        println!("{}", part);
+    }
+    ending();
+}
